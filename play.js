8928b4b14b54eb774552b1b61fbaa575676322a1
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-05 20:15:33 +0000

    conn.on event handler

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log('connected to server');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
