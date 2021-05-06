c48fa01700e5098dc80b022c109823e39390d588
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-06 02:14:08 +0000

    snake game

--- input.js
+++ input.js
@@ -1,11 +1,15 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", (key) => {
-    handleUserInput(key);
-  });
+  // stdin.on("data", (key) => {
+  //   handleUserInput(key);
+  // });
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
@@ -14,8 +18,23 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+
 };
 
-setupInput();
+
+
+// setupInput();
 
 module.exports = { setupInput };
